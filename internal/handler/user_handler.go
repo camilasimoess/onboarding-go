11a90c8a774053af8c9367f7ae28ae0ce1b0aa7c
Camila Simoes <camila.simoes@wexinc.com>
2025-04-09 15:24:00 -0300

    - added logs info/error

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"github.com/camilasimoess/onboarding-go/internal/model"
 	"github.com/camilasimoess/onboarding-go/internal/service"
 	"log/slog"
@@ -34,16 +33,19 @@ func validateRequest(user model.User) error {
 }
 
 func (h *UserHandler) CreateUser(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	slog.Info("saving user")
+	slog.Info("request to create user received")
 	var user model.User
 	err := json.NewDecoder(r.Body).Decode(&user)
+
 	if err != nil {
+		slog.Error("failed to decode request body", slog.String("error", err.Error()))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	err = validateRequest(user)
 	if err != nil {
+		slog.Error("validation failed", slog.String("error", err.Error()))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -52,27 +54,36 @@ func (h *UserHandler) CreateUser(ctx context.Context, w http.ResponseWriter, r *
 	if err != nil {
 		var validationError *service.ValidationError
 		if errors.As(err, &validationError) {
+			slog.Error("service validation failed", slog.String("error", validationError.Error()))
 			http.Error(w, validationError.Error(), http.StatusBadRequest)
 			return
 		}
+		slog.Error("failed to create user", slog.String("error", err.Error()))
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	slog.Info("user created successfully", slog.Any("user", user))
 	w.WriteHeader(http.StatusCreated)
 }
 
 func (h *UserHandler) GetUser(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
-	slog.Info(fmt.Sprintf("finding user with id: %s", id))
+	slog.Info("received request to get user", slog.String("id", id))
 	user, err := h.service.GetUser(ctx, id)
+
 	if err != nil {
+		slog.Error("failed to get user", slog.String("error", err.Error()))
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
+
 	if user == nil {
+		slog.Error("user not found", slog.String("id", id))
 		http.Error(w, "user not found", http.StatusNotFound)
 		return
 	}
+
+	slog.Info("user found", slog.Any("user", user))
 	json.NewEncoder(w).Encode(user)
 }
